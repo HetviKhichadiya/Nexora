02b8e168c5badc5a89cbdc4a0462d47ca9819f22
hetvi-moontechnolabs <hetvi@moontechnolabs.com>
2026-05-11 18:46:06 +0530

    create organizations, roles, organization_users

---
+++ nexora/database/migrations/2026_05_11_131128_crerate_organization_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('organization_users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('organization_id')->constrained('organizations');
+            $table->foreignId('role_id')->constrained('roles');
+            $table->tinyInteger('status')->comment('0: in_active, 1: active');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
