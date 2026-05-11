776970e80f71b4b8afd72cb2493a7733cbb9bbd8
hetvi-moontechnolabs <hetvi@moontechnolabs.com>
2026-05-11 19:00:05 +0530

    create permissions and role_permissions

---
+++ nexora/database/migrations/2026_05_11_132913_create_role_permissions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('role_permissions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('role_id')->constrained('roles');
+            $table->foreignId('permission_id')->constrained('permissions');
+            $table->boolean('permission_value')->comment('0: in_active, 1: active');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('role_permissions');
+    }
+};
