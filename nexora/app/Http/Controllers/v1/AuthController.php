2850d8ae1d4b5cb2d8fea79a604f8eb5d146ab98
hetvi-moontechnolabs <hetvi@moontechnolabs.com>
2026-05-12 18:08:48 +0530

    add common helper and auth controller

---
+++ nexora/app/Http/Controllers/v1/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Signup user
+     */
+    public function signup(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]); 
+    }
+}
