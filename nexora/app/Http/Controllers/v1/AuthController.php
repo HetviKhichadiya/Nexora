42820c3054c56bb5490d9bf73bf366d14e01f769
hetvi-moontechnolabs <hetvi@moontechnolabs.com>
2026-05-20 16:45:15 +0530

    completed auth with token based for login, logou, delete and sign up user

--- nexora/app/Http/Controllers/v1/AuthController.php
+++ nexora/app/Http/Controllers/v1/AuthController.php
@@ -6,6 +6,7 @@ use App\Constants\HttpStatusConstant;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -20,34 +21,38 @@ class AuthController extends Controller
             'password' => 'required|string|min:8',
         ]);
 
-        $email = $request->email;
-        $password = $request->password;
-        $name= $request->name;
-        $first_name = $request->first_name ?? '';
-        $last_name = $request->last_name ?? '';
-        $status = $request->status ?? 1;
-        $user_type = $request->user_type ?? 1;
+        try{
+            $email = $request->email;
+            $password = $request->password;
+            $name = $request->name;
+            $first_name = $request->first_name ?? '';
+            $last_name = $request->last_name ?? '';
+            $status = $request->status ?? 1;
+            $user_type = $request->user_type ?? 1;
 
-        // Check email exists in users table
-        $check_email = emailExists($email);
-        if ($check_email) {
-            return errorResponse(HttpStatusConstant::BAD_REQUEST, 'EMAIL_EXISTS', 'Email already exists');
+            // Check email exists in users table
+            $check_email = emailExists($email);
+            if ($check_email) {
+                return errorResponse(HttpStatusConstant::BAD_REQUEST, 'EMAIL_EXISTS', 'Email already exists');
+            }
+
+            $create_user = User::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => bcrypt($password),
+                'first_name' => $first_name,
+                'last_name' => $last_name,
+                'status' => $status,
+                'user_type' => $user_type,
+            ]);
+
+            if (!$create_user) {
+                return errorResponse(HttpStatusConstant::INTERNAL_SERVER_ERROR, 'INTERNAL_SERVER_ERROR', 'Failed to create user');
+            }
+            return successResponse(HttpStatusConstant::CREATED, $create_user);
+        } catch (\Exception $e) {
+            return errorResponse(HttpStatusConstant::INTERNAL_SERVER_ERROR, 'INTERNAL_SERVER_ERROR', 'An error occurred during signup');
         }
-
-        $create_user = User::create([
-            'name' => $name,
-            'email' => $email,
-            'password' => bcrypt($password),
-            'first_name' => $first_name,
-            'last_name' => $last_name,
-            'status' => $status,
-            'user_type' => $user_type,
-        ]);
-
-        if(!$create_user) {
-            return errorResponse(HttpStatusConstant::INTERNAL_SERVER_ERROR, 'INTERNAL_SERVER_ERROR', 'Failed to create user');
-        }
-        return successResponse(HttpStatusConstant::CREATED, $create_user);
     }
 
     /**
@@ -55,11 +60,61 @@ class AuthController extends Controller
      */
     public function deleteUser($id)
     {
-        $user = User::find($id);
-        if (!$user) {
-            return errorResponse(HttpStatusConstant::NOT_FOUND, 'USER_NOT_FOUND', 'User not found');
+        try{
+            $user = User::find($id);
+            if (!$user) {
+                return errorResponse(HttpStatusConstant::NOT_FOUND, 'USER_NOT_FOUND', 'User not found');
+            }
+            $user->delete();
+            return successResponse(HttpStatusConstant::OK, 'User deleted successfully');
+        } catch (\Exception $e) {
+            return errorResponse(HttpStatusConstant::INTERNAL_SERVER_ERROR, 'INTERNAL_SERVER_ERROR', 'An error occurred while deleting user');
         }
-        $user->delete();
-        return successResponse(HttpStatusConstant::OK, 'User deleted successfully');
     }
+
+    /**
+     * Login user
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        try{
+            $email = $request->email;
+            $password = $request->password;
+            $user = User::where('email', $email)->first();
+            if (!$user) {
+                return errorResponse(HttpStatusConstant::NOT_FOUND, 'USER_NOT_FOUND', 'User not found');
+            }
+            $password_match = Hash::check($password, $user->password);
+            if (!$password_match) {
+                return errorResponse(HttpStatusConstant::UNAUTHORIZED, 'UNAUTHORIZED', 'Invalid credentials');
+            }
+            //generate token for further authentication
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return successResponse(HttpStatusConstant::OK, $token);
+        } catch (\Exception $e) {
+            return errorResponse(HttpStatusConstant::INTERNAL_SERVER_ERROR, 'INTERNAL_SERVER_ERROR', $e->getMessage());
+        }
+    }
+
+    /**
+     * Logout user
+     */
+    public function logout()
+    {
+        try{
+            $user = auth()->user();
+            $logout = $user->currentAccessToken()->delete();
+            if(!$logout) {
+                return errorResponse(HttpStatusConstant::INTERNAL_SERVER_ERROR, 'INTERNAL_SERVER_ERROR', 'Failed to logout');
+            }
+            return successResponse(HttpStatusConstant::OK, 'Logged out successfully');
+        } catch (\Exception $e) {
+            return errorResponse(HttpStatusConstant::INTERNAL_SERVER_ERROR, 'INTERNAL_SERVER_ERROR', 'An error occurred while logging out');
+        }
+    } 
 }
--- nexora/routes/v1/api.php
+++ nexora/routes/v1/api.php
@@ -4,6 +4,12 @@ use App\Http\Controllers\v1\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::post('/signup', [AuthController::class, 'signup']);
-    Route::delete('/user/{id}', [AuthController::class, 'deleteUser']);
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('/signup', 'signup');
+        Route::post('/login', 'login');
+    });
+    Route::controller(AuthController::class)->middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', 'logout');
+        Route::delete('/user/{id}', 'deleteUser');
+    });
 });
